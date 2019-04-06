dda3bc63f42a8cdd910f61f264bd72efb4405d47
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2019-04-06 17:33:24 -0600

    Update WF_lon.py
    I'm adding the function that simulates the WF until some frequency reaches one or zero, basically changing a 'for cycle' for a 'while cycle'

--- WF_lon.py
+++ WF_lon.py
@@ -43,7 +43,27 @@ def WF_lon(d1,a1,d0,a0,n):
         proceso_x0_f.append(1- (numero + ((no_mueren_1 + envejecen1)) + ((no_mueren_0 + envejecen0)))/N)
     return [proceso_x1_I,proceso_x0_I,proceso_x1_f,proceso_x0_f] 
 
-W = WF_lon(0.2,0.3,0.4,0.1,200)
+def WF_lon_completo(d1,a1,d0,a0):
+    proceso_x1_I= [0.25] #x1_I,x0_I,x1_f,x0_f
+    proceso_x0_I = [0.25]
+    proceso_x1_f = [0.25]
+    proceso_x0_f = [0.25]
+    while proceso_x1_I[-1] + proceso_x1_f[-1] != 1 and proceso_x0_I[-1] + proceso_x0_f[-1] != 1:
+        no_mueren_1 = np.random.binomial(N*proceso_x1_I[-1],1-d1)
+        no_mueren_0 = np.random.binomial(N*proceso_x0_I[-1],1-d0)
+        envejecen1 = np.random.binomial(N*proceso_x1_f[-1],a1)
+        envejecen0 = np.random.binomial(N*proceso_x0_f[-1],a0)
+        I =  no_mueren_1 + no_mueren_0 + envejecen1 + envejecen0
+        p = ((1+s1)*proceso_x1_f[-1])/((1+s1)*proceso_x1_f[-1] + (1+s2)*proceso_x0_f[-1])
+        proceso_x1_I.append((no_mueren_1 + envejecen1)/N)
+        proceso_x0_I.append((no_mueren_0 + envejecen0)/N)
+        numero = (np.random.binomial(N-I,p))
+        proceso_x1_f.append(numero/N)
+        proceso_x0_f.append(1- (numero + ((no_mueren_1 + envejecen1)) + ((no_mueren_0 + envejecen0)))/N)
+    return [proceso_x1_I,proceso_x0_I,proceso_x1_f,proceso_x0_f] 
+
+    
+W = WF_lon_completo(0.2,0.3,0.4,0.1)
 Y1 = W[0]
 Y2 = W[1]
 Y3 = W[2]
