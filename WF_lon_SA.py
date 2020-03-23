b246a187939f16e21d5073a5c80b0146f7c57d56
AmilkardelosMonteros <48932036+AmilkardelosMonteros@users.noreply.github.com>
2020-03-22 23:27:18 -0600

    Update and rename WF_lon.py to WF_lon_SA.py
    Ahora ya no se usa multiprossesing pero se usan 20 cores

--- WF_lon_SA.py
+++ WF_lon_SA.py
@@ -8,10 +8,13 @@ Param = int(param)
 N = 100.0
 #La funcion recibe un vector de parametros [s1,a1] y los demas estan fijos
 # Y vamos a seguir a los de tipo1
+x1 = 0.5
+
 def ganador_wf_long(parametros):
     d1 = 1
     s1 = parametros[0]
     a1 = parametros[1]
+    #print('s1:',s1,'a1:',a1)
     d0 = 0
     a0 = 0
     s0 = 0
@@ -20,38 +23,35 @@ def ganador_wf_long(parametros):
     proceso_x1_f = 0.5
     proceso_x0_f = 0.5
     while proceso_x1_f != 0 and proceso_x0_f != 0:
-        no_mueren_1 = np.random.RandomState().binomial(N*proceso_x1_I,1-d1)
-        no_mueren_0 = np.random.RandomState().binomial(N*proceso_x0_I,1-d0)
-        envejecen1 = np.random.RandomState().binomial(N*proceso_x1_f,a1)
-        envejecen0 = np.random.RandomState().binomial(N*proceso_x0_f,a0)
+        no_mueren_1 = np.random.binomial(N*proceso_x1_I,1-d1)
+        no_mueren_0 = np.random.binomial(N*proceso_x0_I,1-d0)
+        envejecen1 = np.random.binomial(N*proceso_x1_f,a1)
+        envejecen0 = np.random.binomial(N*proceso_x0_f,a0)
         I =  no_mueren_1 + no_mueren_0 + envejecen1 + envejecen0
         x1F = proceso_x1_f/(proceso_x1_f + proceso_x0_f)
-        #p = ((1+s1)*proceso_x1_f)/((1+s1)*proceso_x1_f + (1+s0)*proceso_x0_f)
         p = ((1+s1)*x1F)/((1+s1)*x1F + (1+s0)*(1-x1F))
-        proceso_x1_I =  (no_mueren_1 + envejecen1)/N 
-        proceso_x0_I = (no_mueren_0 + envejecen0)/N 
+        proceso_x1_I =  (no_mueren_1 + envejecen1)/N
+        proceso_x0_I = (no_mueren_0 + envejecen0)/N
         numero = np.random.RandomState().binomial(N-I,p)
         proceso_x1_f =  numero/N
         proceso_x0_f =  1- (numero + ((no_mueren_1 + envejecen1)) + ((no_mueren_0 + envejecen0)))/N
     if proceso_x1_f != 0:
         return 1
     else:
-        return 0
+	return 0
 
-#Para que las graficas se puedan leer se fijan 4 parametros: s0,a0,d0,d1 y los demas los variamos en R2 y para cada a1,s1 se estima la probabilidad de fijacion, para cada a1,s1 necesitamos hacer muchas repeticiones y para hacer esto un poco mas rapido usamos la libreria multiprossesing cuidando la aleatoriedad de procesos paralelos, la funcion que hace esto es la siguiente
+#Para que las graficas se puedan leer se fijan 4 parametros: s0,a0,d0,d1 y los demas los variamos en R2 y para cada a1,s1 se estima la probabilidad de fijacion, para cada a1,s1 necesitamos hacer muchas repeticiones y para hacer esto un poco mas rapido usamos 20 cores a la vez
 
 def proba_long(s1,a1,repeticiones):
-    p = Pool()
+    info  = []
     v = [s1,a1]
-    iter = []
     for i in range(repeticiones):
-        iter.append(v)
-    informacion = p.map(ganador_wf_long,iter)
-    return [s1,a1,np.mean(informacion),np.var(informacion)]
+        info.append(ganador_wf_long(v))
+    return [s1,a1,np.mean(info),np.var(info)]
 
 
 def puntos(n,m,repeticiones,numero,nombre):
-    S = np.linspace(-0.025,0.025,n)
+    S = np.linspace(-0.08,0.08,n)
     s = S[numero]
     A = np.linspace(0,1,m)
     archivo = open(nombre,'a')
@@ -59,10 +59,9 @@ def puntos(n,m,repeticiones,numero,nombre):
         WF = proba_long(s,a,repeticiones)
         archivo.write(str(WF) + ',')
     archivo.close()
-    
+
 def funcion(numero):
     nombre = 'un_archivo_longevidad' + str(int(numero)) + '.txt'
-    puntos(20,20,100,numero,nombre)
-
+    puntos(20,20,5000,numero,nombre)
 
 funcion(Param)
